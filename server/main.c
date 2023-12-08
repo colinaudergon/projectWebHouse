3c0352b9dfbb663c27dd95b76b732b207184de27
goblinvision <colinaudergon@gmail.com>
2023-12-08 14:43:15 +0100

    Update main.c

--- server/main.c
+++ server/main.c
@@ -15,10 +15,10 @@
 /*
  *  functions  global:
  *              main
- * 
+ *
  *  functions  local:
  *              shutdownHook
- * 
+ *
  *  Autor	   Elham Firouzi
  *
  ******************************************************************************/
@@ -43,17 +43,23 @@ typedef int int32_t;
 #include "jansson.h"
 #include "Webhouse.h"
 #include "handshake.h"
+#include <string.h>	
 
 //----- Macros -----------------------------------------------------------------
 #define TRUE 1
 #define FALSE 0
+#define TX_BUFFER_SIZE 100
 
 //----- Function prototypes ----------------------------------------------------
-static void shutdownHook (int32_t sig);
+static void shutdownHook(int32_t sig);
+void initSocket(void);
+void sendDataTCP(void);
+void closeConnection(void);
 
 //----- Data -------------------------------------------------------------------
 static volatile int eShutdown = FALSE;
-static int SERVER_PORT_NBR = 8000;
+const SERVER_PORT_NBR = 8000;
+
 //----- Implementation ---------------------------------------------------------
 
 /*******************************************************************************
@@ -67,63 +73,141 @@ static int SERVER_PORT_NBR = 8000;
  *  \return       EXIT_SUCCESS
  *
  ******************************************************************************/
-int main(int argc, char **argv) {
-	int bind_status;
-	int listen_status;
-	int server_sock_id;
-	int com_sock_id;
-	struct sockaddr_in server;
-	struct sockaddr_in client;
-	int addrlen = sizeof (struct sockaddr_in);
-	int backlog = 5;
-	
-	signal(SIGINT, shutdownHook);
+struct sockaddr_in server;
+struct sockaddr_in client;
+int bind_status;
+int listen_status;
+int server_sock_id;
+int com_sock_id;
+int addrlen = sizeof(struct sockaddr_in);
+struct sockaddr_in addr_remote;
+int backlog = 5;
+int newSock_id;
+char txBuf[TX_BUFFER_SIZE];
+int tx_msg_len;
 
-	server.sin_family = AF_INET;
-	server.sin_port = htons(SERVER_PORT_NBR);
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
+int main(int argc, char **argv)
+{
+	signal(SIGINT, shutdownHook);
+	// server.sin_family = AF_INET;
+	// server.sin_port = htons(SERVER_PORT_NBR);
+	// server.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	initWebhouse();
-    printf("Init Webhouse\r\n");
+	printf("Init Webhouse\r\n");
 	fflush(stdout);
+	initSocket();
+	// server_sock_id = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	// if(server_sock_id > 0);
+	// else{
+	// 	printf("Error: socket could not be openedr\r\n");
+	// 	return -1;
+	// }
+	// bind_status = bind(server_sock_id, (struct sockaddr *)&server, addrlen);
+	// if(bind_status > 0);
+	// else{
+	// 	close(server_sock_id);
+	// 	printf("Error: socket could not be bound\r\n");
+	// 	return -1;
+	// }
+	// listen_status = listen(server_sock_id, backlog);
+	// if(listen_status > 0);
+	// else{
+	// 	close(server_sock_id);
+	// 	printf("Error: failed to listen\r\n");
+	// }
 
-	server_sock_id = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(server_sock_id > 0);
-	else{
-		printf("Error: socket could not be openedr\r\n");
-		return -1;
-	}
-
-	bind_status = bind(server_sock_id, (struct sockaddr *)&server, addrlen);
-	if(bind_status > 0);
-	else{
-		close(server_sock_id);
-		printf("Error: socket could not be bound\r\n");
-		return -1;
-	}
-
-	listen_status = listen(server_sock_id, backlog);
-	if(listen_status > 0);
-	else{
-		close(server_sock_id);
-		printf("Error: failed to listen\r\n");
-	}
-
-
-
-	while (eShutdown == FALSE) {
+	while (eShutdown == FALSE)
+	{
 		printf("Main Loop\n");
 		fflush(stdout);
-		
-		//usleep(1000);
+		tx_msg_len = send(newSock_id, &txBuf[0], TX_BUFFER_SIZE, 0);
+
+		const char *message = "Hello world";
+		strncpy(txBuf, message, TX_BUFFER_SIZE);
+		// usleep(1000);
 		sleep(1);
 	}
 
 	closeWebhouse();
-	printf ("Close Webhouse\n");
-	fflush (stdout);
+	printf("Close Webhouse\n");
+	fflush(stdout);
 
 	return EXIT_SUCCESS;
+}
+
+void initSocket(void)
+{
+
+	// Calls binding
+	server.sin_family = AF_INET;
+	server.sin_port = htons(SERVER_PORT_NBR);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// Calls socket
+	server_sock_id = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (server_sock_id > 0)
+		;
+	else
+	{
+		printf("Error: socket could not be openedr\r\n");
+		return -1;
+	}
+
+	// Binds socket
+	bind_status = bind(server_sock_id, (struct sockaddr *)&server, addrlen);
+	if (bind_status > 0)
+		;
+	else
+	{
+		close(server_sock_id);
+		printf("Error: socket could not be bound\r\n");
+		return -1;
+	}
+	// Listening state
+	listen_status = listen(server_sock_id, backlog);
+	if (listen_status > 0)
+	{
+		newSock_id = accept(server_sock_id, (struct sockaddr *)&client,
+							&addrlen);
+		if (newSock_id < 0)
+		{
+			close(server_sock_id);
+		}
+	}
+
+	else
+	{
+		close(server_sock_id);
+		printf("Error: failed to listen\r\n");
+	}
+}
+/*******************************************************************************
+ *  function :    sendDataTCP
+ ******************************************************************************/
+void sendDataTCP(void)
+{
+	tx_msg_len = send(newSock_id, &txBuf[0], TX_BUFFER_SIZE, 0);
+	if (tx_msg_len > 0)
+	{
+		/* Data was sent */
+		printf("Data sent");
+	}
+	else if (tx_msg_len == 0)
+	{
+		/* Connection was lost */
+		printf("Connection lost");
+	}
+	else
+	{
+		/* Data not sent */
+		printf("Data not sent");
+	}
+}
+
+void closeConnection(void)
+{
+	close(newSock_id);
 }
 
 /*******************************************************************************
@@ -138,8 +222,9 @@ int main(int argc, char **argv) {
  *  \return       void
  *
  ******************************************************************************/
-static void shutdownHook(int32_t sig) {
-    printf("Ctrl-C pressed....shutdown hook in main\n");
-    fflush(stdout);
-    eShutdown = TRUE;
+static void shutdownHook(int32_t sig)
+{
+	printf("Ctrl-C pressed....shutdown hook in main\n");
+	fflush(stdout);
+	eShutdown = TRUE;
 }
